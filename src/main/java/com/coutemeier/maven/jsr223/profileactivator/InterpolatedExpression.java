c91b19cb8129b3dfa43c085d3faa18f7733dd64e
Ramón Rial Quintela <rrialq@gmail.com>
2016-11-11 20:36:14 +0100

    Delete duplicated check for isResolved status This commit resolves #3, eliminating the conventional if.

--- src/main/java/com/coutemeier/maven/jsr223/profileactivator/InterpolatedExpression.java
+++ src/main/java/com/coutemeier/maven/jsr223/profileactivator/InterpolatedExpression.java
@@ -10,12 +10,12 @@ package com.coutemeier.maven.jsr223.profileactivator;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -110,9 +110,6 @@ public final class InterpolatedExpression {
      * @since 0.1.0
      */
     public String getString( String defaultValue ) {
-        if ( this.isResolved() ) {
-            return this.expression;
-        }
         return this.isResolved() ? this.expression : defaultValue;
     }
 
